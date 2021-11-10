ad39fcf77197f0a2c656fded88240e37179dd88d
poorpool <489679970@qq.com>
2021-11-10 15:03:10 +0800

    feat(backend): add NotBlank

--- src/main/java/com/example/demo/entity/Rule.java
+++ src/main/java/com/example/demo/entity/Rule.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -15,23 +16,29 @@ public class Rule {
     private Integer aid;
 
     @NotNull(message = "platform 不能为空")
+    @NotBlank(message = "platform 不能为空")
     private String platform;
 
     @NotNull(message = "download_url 不能为空")
+    @NotBlank(message = "download_url 不能为空")
     private String download_url;
 
     @NotNull(message = "update_version_code 不能为空")
+    @NotBlank(message = "update_version_code 不能为空")
     private String update_version_code;
 
     @NotNull(message = "md5 不能为空")
+    @NotBlank(message = "md5 不能为空")
     private String md5;
 
     private String device_id_list;
 
     @NotNull(message = "max_update_version_code 不能为空")
+    @NotBlank(message = "max_update_version_code 不能为空")
     private String max_update_version_code;
 
     @NotNull(message = "min_update_version_code 不能为空")
+    @NotBlank(message = "min_update_version_code 不能为空")
     private String min_update_version_code;
 
     private Integer max_os_api;
@@ -39,15 +46,19 @@ public class Rule {
     private Integer min_os_api;
 
     @NotNull(message = "cpu_arch 不能为空")
+    @NotBlank(message = "cpu_arch 不能为空")
     private String cpu_arch;
 
     @NotNull(message = "channel 不能为空")
+    @NotBlank(message = "channel 不能为空")
     private String channel;
 
     @NotNull(message = "title 不能为空")
+    @NotBlank(message = "title 不能为空")
     private String title;
 
     @NotNull(message = "update_tips 不能为空")
+    @NotBlank(message = "update_tips 不能为空")
     private String update_tips;
 
     @NotNull(message = "status 不能为空")
